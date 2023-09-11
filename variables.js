dafd16f4c87618fe665c9d1cd896a62d13526476
Abraham <Abrahamperezquiroz0@gmail.com>
2023-09-11 11:31:23 -0600

    03 Variables

---
+++ variables.js
@@ -0,0 +1,24 @@
+//versiones anteriores a ES6 era var
+var varibale;
+
+//de ES6 en adelante
+//const y let
+
+//const declara constantes, no permite reasignación
+//Al crearla debe inicializarse
+const PI=3.14;
+//PI=3.28;
+
+//let declara bariables
+//permite reasignación
+//se puede declarar y luego inicializar
+//JS detecta automaticamente el tipo de dato
+let firtsname;
+console.log(firtsname);
+firtsname='Abraham';
+console.log(firtsname);
+console.log(typeof(firtsname));
+
+firtsname=52;
+console.log(firtsname);
+console.log(typeof(firtsname));
