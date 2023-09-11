44bbbb2622fe17de6f333a5f6460f634062fdc1d
Abraham <Abrahamperezquiroz0@gmail.com>
2023-09-11 11:21:01 -0600

    02 Tipos de datos en JS

---
+++ datos.js
@@ -0,0 +1,24 @@
+console.log('Ejecutando desde Node');
+
+//Tipos de datos de JS
+console.log(12);
+console.log(typeof(12));
+
+console.log(12.34);
+console.log(typeof(12.34));
+
+console.log(true);
+console.log(typeof(true));
+
+console.log("string (un conjunto de caracteres)");
+console.log(typeof("string"));
+
+let variable;
+console.log(variable);
+console.log(typeof(variable));
+
+console.log([]);
+console.log(typeof([]));
+
+console.log([1,2,3]);
+console.log({name: 'Abraham'})
