cf84c923d0501d5acefc33dc9ea55d34c4992966
Abraham <Abrahamperezquiroz0@gmail.com>
2023-09-11 11:57:55 -0600

    04 strings

---
+++ strings.js
@@ -0,0 +1,37 @@
+//Strings en Javascript
+
+let string="Texto1 "+'uwu';
+//se puede usar las comillas dobles y simples
+console.log(string);
+
+
+console.log(string.charAt(5));
+console.log(string.endsWith('a'));
+console.log(string.length);
+console.log(string.repeat(2));
+console.log(string.toLowerCase());
+console.log(string.toUpperCase());
+
+
+console.log("I'm Abraham, nice to meet you");
+
+console.log('i\'m Abraham');
+
+//strings multilinea
+const menu=`
+1-opcion 1
+2-opcion 2
+3-opcion 3
+`;
+//altgr+}+espacio
+console.log(menu);
+//strings templates
+//placeholder ${}
+
+const name='Luffy';
+const titulo='El futuro rey de los piratas';
+const age='19';
+
+console.log("Soy "+ name + " y yo sere " + titulo + " y tengo "+ age + "años");
+console.log(`Soy ${name} y yo sere ${titulo} y tengo ${age} años`);
+
